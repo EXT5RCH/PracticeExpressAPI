ab632fad67419fb4048e6f0c369de263353fbc21
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-08-11 21:30:34 +0900

    feat: connection mongo

--- express-api/app/app.mjs
+++ express-api/app/app.mjs
@@ -1,6 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/v1/index.mjs";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(`mongodb://0.0.0.0:${process.env.MONGO_PORT}`, {
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    dbName: process.env.MONGO_INITDB_DATABASE,
+  })
+  .then(() => {
+    console.log(`MongoDB connection success`);
+  })
+  .catch((err) => {
+    console.log(`MongoDB connection error: ${err}`);
+    process.exit(-1);
+  });
 
 const app = express();
 
