2ae5ab2808ed6d925b7ec3c31a3a8a6f08b7110a
satoshi yamada <satoshi.yamada@sint.co.jp>
2022-08-11 22:32:33 +0900

    refactor: api using mongoDB

--- express-api/app/routes/v1/article.mjs
+++ express-api/app/routes/v1/article.mjs
@@ -1,8 +1,36 @@
 import express from "express";
 const router = express.Router();
+import ArticleModel from "../../models/articleModel.mjs";
 
-router.get("/test", (_req, res) => {
-  res.json({ message: "This is article api" });
+router.get("/", (_req, res) => {
+  ArticleModel.find({}, (err, articles) => {
+    err ? res.send(err) : res.json(articles);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const articleId = req.params.id;
+  ArticleModel.findById(articleId, (err, article) => {
+    err ? res.send(err) : res.json(article);
+  });
+});
+
+router.post("/", (req, res) => {
+  const article = new ArticleModel();
+  article.title = req.body.title;
+  article.text = req.body.text;
+  article.setDate();
+
+  article.save((err) => {
+    err ? res.send(err) : res.json({ message: "Article create Success!!" });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const articleId = req.params.id;
+  ArticleModel.remove({ _id: articleId }).then(() => {
+    res.json({ message: "Article delete success!!" });
+  });
 });
 
 export { router };
--- express-api/app/routes/v1/user.mjs
+++ express-api/app/routes/v1/user.mjs
@@ -1,8 +1,46 @@
 import express from "express";
 const router = express.Router();
+import UserModel from "../../models/userModel.mjs";
 
-router.get("/test", (_req, res) => {
-  res.json({ message: "This is user api" });
+router.get("/", (_req, res) => {
+  UserModel.find({}, (err, users) => {
+    err ? res.send(err) : res.json(users);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const userId = req.params.id;
+  UserModel.findById(userId, (err, user) => {
+    err ? res.send(err) : res.json(user);
+  });
+});
+
+router.post("/", (req, res) => {
+  const user = new UserModel();
+  user.name = req.body.name;
+  user.screen_name = req.body.screen_name;
+  user.bio = req.body.bio;
+
+  user.save((err) => {
+    err ? res.send(err) : res.json({ message: "User create Success!!" });
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const userId = req.params.id;
+  UserModel.findById(userId, (err, user) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    user.name = req.body.name;
+    user.screen_name = req.body.screen_name;
+    user.bio = req.body.bio;
+
+    user.save((err) => {
+      err ? res.send(err) : res.json({ message: "User update Success!!" });
+    });
+  });
 });
 
 export { router };
